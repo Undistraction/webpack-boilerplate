6e568cd8bc69301b317cc8ad0f6ebd95011a9e02
Pedr Browne <pedr.browne@gmail.com>
2017-01-30 20:02:59 +0000

    build: Add React HMR

---
+++ app/js/components/Comp.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class Comp extends React.Component {
+
+  render() {
+    if (this.props.name === null) {
+      return null;
+    }
+
+    return (
+      <div className="Comp">
+        <h1 className="Comp-title">{`${this.props.name} is my name`}</h1>
+      </div>
+    );
+  }
+}
+
+Comp.propTypes = {
+  name: React.PropTypes.string.isRequired
+};
