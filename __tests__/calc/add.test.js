1048762fa3d653db3a995e20fe351b652115036f
Pedr Browne <pedr.browne@gmail.com>
2017-02-03 21:41:29 +0000

    test: Add a few more tests

--- __tests__/calc/add.test.js
+++ __tests__/calc/add.test.js
@@ -1,5 +1,16 @@
 import add from '../../app/js/calc/add';
 
-it('Adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+
+describe('addition', () => {
+  it('Adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+  });
+
+  it('Adds -1 + -5 to equal -6', () => {
+    expect(add(-1, -5)).toBe(-6);
+  });
+
+  it('Adds 0 + 0 to equal 0', () => {
+    expect(add(0, 0)).toBe(0);
+  });
 });
