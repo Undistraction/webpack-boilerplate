1a694e2481a6f6e241c0c92fa579ccac300c782a
Pedr Browne <pedr.browne@gmail.com>
2018-03-15 14:11:46 +0000

    feat: Add gui

--- src/js/animations/render.js
+++ src/js/animations/render.js
@@ -6,21 +6,52 @@ const randomIntInRangeSeeded = randomIntInRange(seedrandom(1))
 const midPointX = context => context.canvas.clientWidth * 0.5
 const midPointY = context => context.canvas.clientHeight * 0.5
 
-const testAnimation = context => () => {
-  // From Context
+const testAnimation = (context, gui) => {
+  // ---------------------------------------------------------------------------
+  // Cache fixed data
+  // ---------------------------------------------------------------------------
   const canvasWidth = context.canvas.clientWidth
   const canvasHeight = context.canvas.clientHeight
   const canvasMidX = midPointX(context)
   const canvasMidY = midPointY(context)
-  // Set Context
-  const strokeColor = `rgb(220, 220, 220)`
-  context.strokeStyle = strokeColor
+  const toRGBString = a =>
+    `rgb(${Math.round(a[0])}, ${Math.round(a[1])}, ${Math.round(a[2])})`
+
+  // ---------------------------------------------------------------------------
+  // Defined fixed vars
+  // ---------------------------------------------------------------------------
   context.lineJoin = `round`
   context.lineCap = `round`
-  context.shadowBlur = 4
-  context.shadowColor = strokeColor
+
+  // ---------------------------------------------------------------------------
+  // Defined tweakable vars
+  // ---------------------------------------------------------------------------
+  const vars = {
+    shadowBlur: 4,
+    strokeColor: [220, 220, 220],
+  }
+
+  // ---------------------------------------------------------------------------
+  // Configure GUI
+  // ---------------------------------------------------------------------------
+
+  // Shadow Blur
+  gui
+    .add(vars, `shadowBlur`)
+    .min(0)
+    .max(120)
+
+  // Shadow Colour
+  gui.addColor(vars, `strokeColor`)
+  // Allow save
+  gui.remember(vars)
 
   const render = (startX, startY) => {
+    context.shadowBlur = vars.shadowBlur
+    const strokColorString = toRGBString(vars.strokeColor)
+    context.shadowColor = strokColorString
+    context.strokeStyle = strokColorString
+    console.log(strokColorString)
     const x = randomIntInRangeSeeded(0, canvasWidth)
     const y = randomIntInRangeSeeded(0, canvasHeight)
     context.beginPath()
--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import dat from 'dat.gui'
 import render from './animations/render'
 
 class Renderer extends React.Component {
   componentDidMount() {
+    // -------------------------------------------------------------------------
+    // Tooling Setup
+    // -------------------------------------------------------------------------
+
+    const gui = new dat.GUI({ autoPlace: false })
+    document.getElementById(`gui`).appendChild(gui.domElement)
+
+    // -------------------------------------------------------------------------
+    // Canvas Setup
+    // -------------------------------------------------------------------------
+
     const context = this.canvas.getContext(`2d`)
-    this.animationID = window.requestAnimationFrame(render(context))
+    this.animationID = window.requestAnimationFrame(() => render(context, gui))
   }
 
   componentWillUnmount() {
@@ -13,13 +25,16 @@ class Renderer extends React.Component {
 
   render() {
     return (
-      <canvas
-        ref={canvas => {
-          this.canvas = canvas
-        }}
-        width={window.innerWidth}
-        height={window.innerHeight}
-      />
+      <div>
+        <canvas
+          ref={canvas => {
+            this.canvas = canvas
+          }}
+          width={window.innerWidth}
+          height={window.innerHeight}
+        />
+        <div id="gui" />
+      </div>
     )
   }
 }
